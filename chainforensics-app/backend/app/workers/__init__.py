882370cdaa9f2a4e526ba47b4f4fc0ddddc378e7
manic-startup <81828167+manic-startup@users.noreply.github.com>
2026-01-02 10:47:00 +0000

    Delete chainforensics-app directory

--- chainforensics-app/backend/app/workers/__init__.py
+++
@@ -1,4 +0,0 @@
-# ChainForensics Workers
-from app.workers.indexer import BackgroundIndexer, get_indexer, start_background_indexer
-
-__all__ = ["BackgroundIndexer", "get_indexer", "start_background_indexer"]
